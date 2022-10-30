29ae884011b4369525f1fde4982d4b8f8d585706
Sirox <71220271+Sirox228@users.noreply.github.com>
2022-10-30 14:32:34 +0300

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -6,6 +6,8 @@ FLAGS="--enable-assertions=yes --disable-examples --disable-doc"
 ./configure $FLAGS
 cd ../
 
+echo ${PKG_CONFIG_PATH}
+
 echo "starting"
 
 ${NDK}/ndk-build NDK_PROJECT_PATH=./ NDK_APPLICATION_MK=./Application.mk APP_BUILD_SCRIPT=./Android.mk
